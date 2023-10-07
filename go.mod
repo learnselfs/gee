fca718fc243492178311b7d72804b886362b59f8
learnselfs <learnselfs@outlook.com>
2023-10-07 14:48:44 +0800

    v1.2   - 修改了main中引用core   - 添加了 mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/learnselfs/gee
+
+go 1.21.1
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 // Package gee @Author Bing
 // @Date 2023/9/26 21:09:00
-// @Desc
+// @Describe gee main function
 package gee
 
-import "gee/core"
+import "github.com/learnselfs/gee/core"
 
 func New(address, port string) *core.Engine {
 	return &core.Engine{Address: address, Port: port}
